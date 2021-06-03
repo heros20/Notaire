4da808debb94aebba2bba32ee63691068316028a
ZenonStella <stellazenon@gmail.com>
2021-06-03 15:53:32 +0200

    controller

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -24,11 +24,25 @@ class ProfileController extends AbstractController
             'controller_name' => 'ProfileController',
         ]);
     }
+    #[Route('/notification/show', name: 'notif_show')]
+    public function notifShow(): Response
+    {
+        return $this->render('profile/notification_show.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
     
     #[Route('/informations_personnelles', name: 'infos_perso')]
     public function infos_preso(): Response
     {
-        return $this->render('profile/informations_personnelles.html.twig', [
+        return $this->render('profile/infos_perso.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    #[Route('/informations_personnelles/edit', name: 'infos_perso_edit')]
+    public function infos_preso(): Response
+    {
+        return $this->render('profile/infos_perso_edit.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
     }
@@ -37,6 +51,20 @@ class ProfileController extends AbstractController
     public function favori(): Response
     {
         return $this->render('profile/favoris.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    #[Route('/favoris/show', name: 'favoris_show')]
+    public function favori(): Response
+    {
+        return $this->render('profile/favoris_show.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    #[Route('/favoris/edit', name: 'favoris_edit')]
+    public function favori(): Response
+    {
+        return $this->render('profile/favoris_edit.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
     }
