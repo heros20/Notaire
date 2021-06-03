d8cdaa2637feeaf4b873fb6aa0a83767b18a32a2
ZenonStella <stellazenon@gmail.com>
2021-06-03 15:09:43 +0200

    escpace client routes

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/profile')]
 class ProfileController extends AbstractController
 {
-    #[Route('/profile', name: 'profile')]
+    #[Route('/', name: 'profile')]
     public function index(): Response
     {
         return $this->render('profile/index.html.twig', [
