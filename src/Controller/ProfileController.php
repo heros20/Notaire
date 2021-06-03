9973da2c3125084e60b37c792e87e1dbc325f14d
ZenonStella <stellazenon@gmail.com>
2021-06-03 15:00:32 +0200

    escpace client template profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -15,4 +15,28 @@ class ProfileController extends AbstractController
             'controller_name' => 'ProfileController',
         ]);
     }
+
+    #[Route('/notification', name: 'notif')]
+    public function notif(): Response
+    {
+        return $this->render('profile/notification.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    
+    #[Route('/informations_personnelles', name: 'infos_perso')]
+    public function infos_preso(): Response
+    {
+        return $this->render('profile/informations_personnelles.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+    
+    #[Route('/favoris', name: 'favoris')]
+    public function favori(): Response
+    {
+        return $this->render('profile/favoris.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
 }
