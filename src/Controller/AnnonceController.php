e6c095f0eec251f04fd02ea26fa4b1b4d55c53b7
Seb2197 <sebastienweb27@gmail.com>
2021-06-03 16:55:00 +0200

    Mise en page de l'admin 4/7

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/Admin/Annonce')]
+#[Route('/admin/annonce')]
 class AnnonceController extends AbstractController
 {
     #[Route('/', name: 'annonce_index', methods: ['GET'])]
