12420cb3f9805b9618c84755803b06a64ba0549c
ZenonStella <stellazenon@gmail.com>
2021-06-05 19:45:11 +0200

    forms relationnels

--- src/Form/VilleType.php
+++ src/Form/VilleType.php
@@ -3,15 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Ville;
-use App\Entity\Departement;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
 class VilleType extends AbstractType
 {
@@ -26,13 +23,6 @@ class VilleType extends AbstractType
             'label' => 'Code Postal',
             'attr' => ['placeholder' => 'Code postal de la ville...']
         ])
-        ->add('departement', EntityType::class, [
-            'label' => 'Déparetement *',
-            'class' => Departement::class,
-            'choice_label' => 'title',
-            // 'multiple' => true,
-            // 'expanded' => true,
-        ])
         ->add('description', TextareaType::class, [
             'label' => 'Description ( Optionnel )',
             'attr' => ['placeholder' => 'Description de la ville...']
@@ -43,7 +33,7 @@ class VilleType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Departement::class,
+            'data_class' => Ville::class,
         ]);
     }
 }
