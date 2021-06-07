5e7407f8af5898515f2b81e35efe504850d1251a
kevin bigoni <heros40@hotmail.fr>
2021-06-07 10:04:37 +0200

    affichage favoris

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Annonce;
+
 
 class HomeController extends AbstractController
 {
