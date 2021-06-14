5cb05a1d9602fffdfbdd3a88dbd1040d23a57da3
kevin bigoni <heros40@hotmail.fr>
2021-06-14 09:03:19 +0200

    pull from portable

--- migrations/Version20210613232818.php
+++ migrations/Version20210613232818.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210613162032 extends AbstractMigration
+final class Version20210613232818 extends AbstractMigration
 {
     public function getDescription(): string
     {
