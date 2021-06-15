dd6639c07ac2b0f66e9b584b0337af69c6e3a5bc
Seb2197 <sebastienweb27@gmail.com>
2021-06-15 12:17:37 +0200

    Finitions du site

--- src/DataFixtures/AnnonceFixtures.php
+++ src/DataFixtures/AnnonceFixtures.php
@@ -14,7 +14,6 @@ class AnnonceFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-<<<<<<< HEAD
         $annonce1 = new Annonce();
         $annonce1->setTitle('Villa');
         $annonce1->setDescription('Découvrez cette magnifique villa au bord de mer');
@@ -35,84 +34,6 @@ class AnnonceFixtures extends Fixture implements DependentFixtureInterface
         $annonce1->setDepartement($this->getReference('departement1'));
         $annonce1->addCategory($this->getReference('category1'));
         $manager->persist($annonce1);
-=======
-        for ($i=0; $i < 2; $i++) { 
-            $annonce1 = new Annonce();
-            $annonce1 ->setTitle('Villa vue sur mer');
-            $annonce1 ->setDescription('jolie trop mognon');
-            $annonce1 ->setImage('paysage.jpg');
-            $annonce1 ->setPrice(120000);
-            $annonce1 ->setSuperficie(1000);
-            $annonce1 ->setSuperficieTerrain(1500);
-            $annonce1 ->setDpe('A');
-            $annonce1 ->setGes('B');
-            $annonce1 ->setNbrePieces(4);
-            $annonce1 ->setNbreChambre(2);
-            $annonce1 ->setSalleBain(1);
-            $annonce1 ->setWc(1);
-            $annonce1 ->setGarage('non');
-            $annonce1 ->setPiscine('non');
-            $annonce1 ->setStatus(true);
-            $annonce1 ->setVille($this->getReference('ville1'));
-            $annonce1 ->setDepartement($this->getReference('departement2'));
-            $annonce1 ->addCategory($this->getReference('category3'));
-            $manager->persist($annonce1);
-        }
-        for ($i=0; $i < 2; $i++) { 
-            $annonce2 = new Annonce();
-            $annonce2 ->setTitle('knrfsvrs');
-            $annonce2 ->setImage('https://picsum.photos/200/300');
-            $annonce2 ->setDescription('livlsfhrvr fvjiovjrfs dsxilvsfjvsdosoivs');
-            $annonce2 ->setPrice(20000);
-            $annonce2 ->setSuperficie(1000);
-            $annonce2 ->setSuperficieTerrain(1500);
-            $annonce2 ->setDpe('C');
-            $annonce2 ->setGes('D');
-            $annonce2 ->setNbrePieces(4);
-            $annonce2 ->setNbreChambre(2);
-            $annonce2 ->setSalleBain(1);
-            $annonce2 ->setWc(1);
-            $annonce2 ->setGarage('oui');
-            $annonce2 ->setPiscine('oui');
-            $annonce2 ->setStatus(false);
-            $annonce2 ->setVille($this->getReference('ville1'));
-            $annonce2 ->setDepartement($this->getReference('departement1'));
-            $annonce2 ->addCategory($this->getReference('category2'));
-            $manager->persist($annonce2);
-        }
-        for ($i=0; $i < 2; $i++) { 
-            $annonce3 = new Annonce();
-            $annonce3 ->setTitle('fjfzrisf ffikfg');
-            $annonce3 ->setDescription('jhforif zhfifes fhesd qfrfzr');
-            $annonce3 ->setImage('https://picsum.photos/200/300');
-            $annonce3 ->setPrice(100000);
-            $annonce3 ->setSuperficie(1000);
-            $annonce3 ->setSuperficieTerrain(1500);
-            $annonce3 ->setDpe('E');
-            $annonce3 ->setGes('F');
-            $annonce3 ->setNbrePieces(4);
-            $annonce3 ->setNbreChambre(2);
-            $annonce3 ->setSalleBain(1);
-            $annonce3 ->setWc(1);
-            $annonce3 ->setGarage('non');
-            $annonce3 ->setPiscine('oui');
-            $annonce3 ->setStatus(true);
-            $annonce3 ->setVille($this->getReference('ville2'));
-            $annonce3 ->setDepartement($this->getReference('departement3'));
-            $annonce3 ->addCategory($this->getReference('category1'));
-            $manager->persist($annonce3);
-        }
-        // $annonce1 = new Annonce();
-        // $annonce1 ->setTitle('Villa vue sur mer');
-        // $annonce1 ->setDescription('jolie trop mognon');
-        // $annonce1 ->setImage('https://picsum.photos/200/300');
-        // $annonce1 ->setPrice(120000);
-        // $annonce1 ->setStatus(true);
-        // $annonce1 ->setVille($this->getReference('ville1'));
-        // $annonce1 ->setDepartement($this->getReference('departement2'));
-        // $annonce1 ->addCategory($this->getReference('category3'));
-        // $manager->persist($annonce1);
->>>>>>> 106b3692a71a3d1552c9e7912220b613ccca4035
 
         $annonce2 = new Annonce();
         $annonce2->setTitle('Villa');
