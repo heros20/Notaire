f6c2143254d28f48c45a7e6ed1391573f5949093
heros20 <heros40@hotmail.fr>
2021-06-15 23:01:51 +0200

    pull from tower

--- assets/front.js
+++ assets/front.js
@@ -76,27 +76,7 @@ if (slider) {
   });
 }
 
-if ($('#footer').height() > $(".filter").height()) {
-  $('.filter').attr('style', 'display: none!important; bottom: 0px;');
-}
 
-let modal = document.getElementById("myModal");
-let btn = document.getElementById("myBtn");
-let span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
 
 
 
