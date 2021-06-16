39fd89c00460b6a7d657e304d3548313d3ea243c
ZenonStella <stellazenon@gmail.com>
2021-06-16 10:59:37 +0200

    email footer

--- migrations/Version20210616083408.php
+++ migrations/Version20210616083408.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210616075007 extends AbstractMigration
+final class Version20210616083408 extends AbstractMigration
 {
     public function getDescription(): string
     {
